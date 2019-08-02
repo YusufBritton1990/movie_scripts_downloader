06b560d14dee07cc30732da344bdca18606233a7
Yusuf Britton <yusufbritton@yahoo.com>
2019-08-02 08:50:47 -0400

    Successful requested all movies

--- springfield-requests.py
+++ springfield-requests.py
@@ -20,7 +20,7 @@ alphabetHTML = soup.find('div', {'class': "search-and-alpha"})
 # Receive just the links
 alphabetPages = alphabetHTML.find_all('a')
 
-# # Normal List from a for loop
+# # Normal List from a for loop, to understand how list comprehension works
 # orderList = []
 # for link in alphabetPages:
 #
@@ -30,46 +30,68 @@ alphabetPages = alphabetHTML.find_all('a')
 
 # List comprehension
 orderList = [link.text for link in alphabetPages]
-# print(orderList)
 
-"""Requesting movie scripts from page count interable"""
-PaginationHTML = soup.find('div', {'class': "pagination"})
-# print(PaginationHTML.prettify())
 
-Pagination = PaginationHTML.find_all('a')
+"""Page scrape"""
+# """Requesting movie scripts from page count interable"""
+# PaginationHTML = soup.find('div', {'class': "pagination"})
+# # print(PaginationHTML.prettify())
+#
+#
+# Pagination = PaginationHTML.find_all('a')
+#
+# # Retrieve the amount of pages of movies on each alphabet page
+# lastPage = int([link.text for link in Pagination][-1])
+# # print(type(lastPage))
+#
+# for page in range(1,lastPage + 1):
+#     url = url + f"&page={page}"
+#     print(url)
 
-# Retrieve the amount of pages of movies on each alphabet page
-lastPage = int([link.text for link in Pagination][-1])
-print(type(lastPage))
-
+"""Final Product"""
+# TODO: There seems to be an error, there are duplicates.
+# Lets do one instances
 """Scraping each movie title on each page in each alphabet order page"""
 for letter in orderList:
     # Using requests, start on the initial page number of the alphabet order
-    url = f"https://www.springfieldspringfield.co.uk/movie_scripts.php?order={letter}&page=1"
-    res = requests.get(url)
+    urlOrder = f"https://www.springfieldspringfield.co.uk/movie_scripts.php?order={letter}"
+    res = requests.get(urlOrder, timeout=20)
     # print(url)
 
     soup = BeautifulSoup(res.text, "lxml") #Using requests, Type is a soup
 
-    # This is a list that contains all the information in the soup,
-    # which contains the movies titles and years
-    outer_box = soup.find('div', {'class': "main-content-left"})
+    """Requesting movie scripts from page count interable"""
+    PaginationHTML = soup.find('div', {'class': "pagination"})
+    # print(PaginationHTML.prettify())
 
-    # This is a list of the movie titles and years
-    movie_titles_list = outer_box.find_all('a', {'class': "script-list-item"})
+    Pagination = PaginationHTML.find_all('a')
 
-    """Getting the number page"""
+    # Retrieve the amount of pages of movies on each alphabet page
+    lastPage = int([link.text for link in Pagination][-1])
+    # print(type(lastPage))
 
+    for page in range(1,lastPage + 1):
+        urlPage = urlOrder + f"&page={page}"
+        res = requests.get(urlPage, timeout=10)
 
-    """Parsing out movies on each number page"""
-    for movie in movie_titles_list:
-        # for page in paginationList:
-        full_name = movie.text #The actually movie title and year
+        soup = BeautifulSoup(res.text, "lxml") #Using requests, Type is a soup
 
-        # spline full_name into the movie name and year.
-        #for year, convert to an interger and not include parentheses
-        name, year = full_name[:-6], int(full_name[-5:-1])
-        print(name, year)
+        # This is a list that contains all the information in the soup,
+        # which contains the movies titles and years
+        outer_box = soup.find('div', {'class': "main-content-left"})
+
+        # This is a list of the movie titles and years
+        movie_titles_list = outer_box.find_all('a', {'class': "script-list-item"})
+
+        """Parsing out movies on each number page"""
+        for movie in movie_titles_list:
+            # for page in paginationList:
+            full_name = movie.text #The actually movie title and year
+
+            # spline full_name into the movie name and year.
+            #for year, convert to an interger and not include parentheses
+            name, year = full_name[:-6], int(full_name[-5:-1])
+            print(name, year)
 
 end = time.time()
 print(end - start)
